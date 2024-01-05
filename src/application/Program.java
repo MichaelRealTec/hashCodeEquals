d189dafe5d3467ac1ff7d48a481bec583b9dc3a3
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-01-05 16:40:07 -0300

    operacao equals

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,17 @@
 package application;
 
 public class Program {
-
+	public static void main(String[] args) {
+		// São operações da clase Object utilizadasa para comparar se um objeto é igual a outro
+		// equals: lento, resposta 100%
+		// hashCode: rápido, porém resposta positiva não é 100%
+		// Tipos comuns (String, Date, Integer, Double, etc.) já possuem implementação para essas operações. Classes personalizadas precisam sobrepô-las.
+		
+		String a = "Maria";
+		String b = "Alex";
+		
+		System.out.println(a.equals(b)); // Resposta = false 
+		
+		
+	}
 }
